eb7fd39b7ead805f7b0bb42a16c51f5c886d70b3
Andryk Lara <lara.cordero@gmail.com>
2020-09-30 12:09:24 -0600

    Cambios ya Ordenados

--- js/script.js
+++ js/script.js
@@ -1,34 +1,61 @@
 
 // Lista de tareas
 
-const listaTareas = document.getElementById('new-task-form');
-const listContainer = document.getElementById('task-list');
+//Modelo
+
 const tareas = [];
+
 let contadorTareas = 0;
 
-listaTareas.addEventListener('submit', (event) => {
-  event.preventDefault();
+function addTask(nombreTarea,fechaTarea, completoTarea){
+  const misTareas = {
+    id: contadorTareas,
+    nombre: nombreTarea,
+    fecha: fechaTarea,
+    completo: completoTarea,
+  };
+
+  tareas.push(misTareas);
+  contadorTareas++;
+
+  return misTareas;
+}
+
+//Vista
+
+const listContainer = document.getElementById('task-list');
+
+function appendTaskDOM(tarea){
   const list = document.createElement('li');
   list.className = 'task-list_li';
   //Checkbox
   const checkbox = document.createElement('input');
   checkbox.setAttribute('type', 'checkbox');
-  checkbox.setAttribute('id', `tarea-${contadorTareas}`);
+  checkbox.setAttribute('id', `tarea-${tarea.id}`);
   //Label
   const label = document.createElement('label');
-  label.setAttribute('for', `tarea-${contadorTareas}`);
-  label.innerHTML = listaTareas.elements[0].value;
+  label.setAttribute('for', `tarea-${tarea.id}`);
+  label.innerHTML = `${tarea.nombre} - ${tarea.fecha}`;
   //Agrego Elementos
   list.appendChild(checkbox);
   list.appendChild(label)
   listContainer.appendChild(list);
-  const misTareas = {
-    id: contadorTareas,
-    nombre: listaTareas.elements[0].value,
-    completo: false,
-  };
-  tareas.push(misTareas);
-  console.log(tareas);
-  contadorTareas++;
+}
+
+//Controlador
+
+const listaTareas = document.getElementById('new-task-form');
+
+
+listaTareas.addEventListener('submit', (event) => {
+  event.preventDefault();
+  
+  const tarea = addTask(listaTareas.elements[0].value,listaTareas.elements[1].value, false)
+  appendTaskDOM(tarea);
+  
   listaTareas.elements[0].value = '';
 })
+
+
+
+
